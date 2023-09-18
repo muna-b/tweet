1072889c194b9725c090ba4376552b301bd86702
muna-b <mainmouna.sakho@gmail.com>
2023-09-18 11:23:09 +0200

    header

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ module.exports = {
     dbUrl: 'mongodb+srv://mainmounasakho:Hassan2014@test.kklz6qg.mongodb.net/?retryWrites=true&w=majority',
     cert: '/etc/letsencrypt/live/www.mainmounasakho.site/fullchain.pem',
     key: '/etc/letsencrypt/live/www.mainmounasakho.site/privkey.pem',
-    portHttp: 80,
+    portHttp: 8080,
     portHttps: 443
 }
