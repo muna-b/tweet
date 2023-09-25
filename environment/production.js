e066a88a047d0dadc41a50f19663a283c6d93b21
muna-b <mainmouna.sakho@gmail.com>
2023-09-25 22:06:05 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ module.exports = {
     dbUrl: 'mongodb+srv://mainmounasakho:Hassan2014@test.kklz6qg.mongodb.net/?retryWrites=true&w=majority',
     cert: '/etc/letsencrypt/live/www.mainmounasakho.site/fullchain.pem',
     key: '/etc/letsencrypt/live/www.mainmounasakho.site/privkey.pem',
-    portHttp: 8080,
+    portHttp: 80,
     portHttps: 443
 }
