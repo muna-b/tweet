54fb215564bfaaf7ffe933e76f9acec513df8559
muna-b <mainmouna.sakho@gmail.com>
2024-02-19 23:54:26 +0100

    certificat

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,7 @@
 module.exports = {
     dbUrl: 'mongodb+srv://mainmounasakho:Hassan2014@test.kklz6qg.mongodb.net/?retryWrites=true&w=majority',
-    cert: '',
-    key: '',
+    cert: '/etc/letsencrypt/live/www.mainmounasakho.site/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.mainmounasakho.site/privkey.pem',
     portHttp: 80,
     portHttps: 443
 }
