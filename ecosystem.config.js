db4f6d104ba853e88489f05c285394b7d7303c2f
muna-b <mainmouna.sakho@gmail.com>
2024-02-26 11:25:30 +0100

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    name: 'tweet',
+    script: './bin/www',
+    instances: 'max',
+    watch: 'true',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
